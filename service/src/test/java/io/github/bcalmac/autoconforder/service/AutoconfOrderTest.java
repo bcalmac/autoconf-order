2fc52b0e4512fb87529c15b87723d044c5b9e5a3
Bogdan Calmac <bogdan.calmac@clearcapital.com>
2018-10-06 17:31:49 -0700

    Initial commit

---
+++ service/src/test/java/io/github/bcalmac/autoconforder/service/AutoconfOrderTest.java
@@ -0,0 +1,26 @@
+package io.github.bcalmac.autoconforder.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoconfOrderTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    public void dbHealthIndicator() {
+        // The assertion below will fail.
+        // Fix by removing @EnableAutoConfiguration from AutoconfOrderStarterConfiguration
+        assertNotNull(applicationContext.getBean("dbHealthIndicator"));
+    }
+
+}
---
+++ starter/src/main/java/io/github/bcalmac/autoconforder/starter/MyStarterConfiguration.java
@@ -0,0 +1,10 @@
+package io.github.bcalmac.autoconforder.starter;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class MyStarterConfiguration {
+
+}
